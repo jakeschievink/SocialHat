c7d22284a351347d2d2ed4e324c47c8527927f34
Jake Schievink <jakeschievink@gmail.com>
2014-02-04 09:06:53 -0500

    Started functionality for setting options through serial

--- SocialHat.ino
+++ SocialHat.ino
@@ -11,28 +11,30 @@
 #define OLED_RESET 13
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 typedef enum {SIDEWAYS, UPWARDS} scroll_type;
+
 typedef struct config_s
 {
     scroll_type scrolling; 
     int text_size;
 }configuration;
 
-configuration cf= {UPWARDS, 2};
+configuration cf= {UPWARDS, 4};
 
-char message[] = "Plants, Computer Programming, Cycling";
+char message[200];
+
 int  x, minX, y, minY;
-String inputString;
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC);
   display.setTextSize(cf.text_size);
   display.setTextColor(WHITE);
   EEPROM_readAnything(0, message);
+
   if(cf.scrolling == SIDEWAYS){
     display.setTextWrap(false);
   }else{
     display.setTextWrap(true);
-    number_of_newlines(message);
+    convert_comma_to_newline(message);
   }
   x = display.width();
   y = display.height();
@@ -53,41 +55,61 @@ void loop() {
   }
   display.print(message);
   display.display();
-  if(Serial.available() > 0){
-    recieveChars().toCharArray(message, 100);
-    Serial.print(message);
-    EEPROM_writeAnything(0, message);
-    number_of_newlines(message);
-    setMinX();
-    setMinY();
-
-  }
+  checkSerial();
+  
 }
+void checkSerial(){
+    bool firstRead = false;
+    char option;
+    if(Serial.available() > 0){
+        if(!firstRead){
+            option = Serial.read();
+            firstRead = true;
+            Serial.println(option);
+        }
+        recieveChars().toCharArray(message, 100);
+        Serial.print(message);
+        EEPROM_writeAnything(0, message);
+        convert_comma_to_newline(message);
+        setMinX();
+        setMinY();
 
+    }
+}
 void setMinX(void){
   minX = -(cf.text_size*6) * strlen(message);   // 12 = 6 pixels/character * text size 2
 }
 void setMinY(void){
-    minY= -((((cf.text_size*6) * strlen(message))/128)*(cf.text_size*7))-display.height();
+    minY= -((get_newlines(message))*(cf.text_size*7))-display.height();
 }
-
-int number_of_newlines(char* msg){
+int get_newlines(char* msg){
     int length = strlen(msg);
-    int newlines = 0; 
+    int newlines = 0;
+    for(int i = 0; i<length; i++){
+        if((byte)msg[i] == 10){
+            Serial.print("newline");
+            newlines++;
+        }
+    }
+    return newlines;
+}
+void convert_comma_to_newline(char* msg){
+    int length = strlen(msg);
     for(int i =0; i<length; i++){
         if((byte)msg[i] == 44){
             msg[i] = ' ';
+            if(msg[i+1] == 44){
+                msg[i] = '\n';
+            }
             msg[i+1] = '\n';
             
         }
     }
-    return newlines;
 }
 
 String recieveChars(){
   String content = "";
   char character;
-
   while(Serial.available()) {
       character = Serial.read();
       content.concat(character);
