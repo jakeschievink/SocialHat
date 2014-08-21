c8620fd66dee61ecaf4cfb38974080f4469ca889
Jake Schievink <jakeschievink@gmail.com>
2014-08-20 21:50:03 -0400

    Added

--- SocialHat.ino
+++ SocialHat.ino
@@ -1,68 +1,32 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <EEPROM.h>
-#include "EEPROMAnything.h"
-#include "Scroll.h"
-#include "Pictures.cpp"
-Scroll scrollscreen(9, 10, 11, 13, 12) ;
-char message[200];
-bool imagedisplayed = false;
-typedef enum {SCROLL, IMAGE} mode;
+#include "ST7565.h"
+#include "thankyouBMP.h"
+#include "glasses.h"
 
-mode currentmode = SCROLL;
-void setup() {
-    Serial.begin(9600);
-    scrollscreen.init();
-    EEPROM_readAnything(1, message);
-    scrollscreen.setMessage(message);
-    Serial.println(message);
-    
+#define BACKLIGHT_LED 10
 
-}
+#define DEBUG 1
+ST7565 glcd(23, 22, 21, 20, 19);
 
-void loop() {
-    while(currentmode == SCROLL){
-        scrollscreen.startScroll();
-        checkSerial();
-    }
-    while(currentmode == IMAGE && !imagedisplayed){
-        displayImage();
-        imagedisplayed = true;
-        
-    }
+void setup(){
+  Serial.begin(9600);
+  glcd.begin(0x18); 
+  
+  glcd.setpixel(10, 10, BLACK);
+  glcd.display();        // show the changes to the buffer
+  delay(2000);
+  glcd.clear();
+  glcd.display();
+  delay(2000);
+  glcd.clear();
+  
+  glcd.drawbitmap(0,0,thankyou, 128,64, BLACK);
+  glcd.display();
+  delay(2000);
+  glcd.clear();
+  
+  glcd.drawbitmap(0,0,glasses, 128,64, BLACK);
+  glcd.display();
+  delay(2000);
 }
-void displayImage(){
-    scrollscreen.disp.clearDisplay();
-    scrollscreen.disp.drawBitmap(0, 0,  oledtest, 128, 64, 1);
-    scrollscreen.disp.display();
-}
-void checkSerial(){
-    bool firstRead = false;
-    char option;
-    char tmpMsg[200];
-    if(Serial.available() > 0){
-        if(!firstRead){
-            option = Serial.read();
-            firstRead = true;
-            Serial.println(option);
-        }
-        recieveChars().toCharArray(tmpMsg, 100);
-        Serial.print(tmpMsg);
-        scrollscreen.setMessage(tmpMsg);
-        Serial.println(EEPROM_writeAnything(1, tmpMsg));
-    }
-}
-
-String recieveChars(){
-  String content = "";
-  char character;
-  while(Serial.available()) {
-      character = Serial.read();
-      content.concat(character);
-      Serial.println((byte)character);
-  }
-  if(content != ""){
-    return content;
-  }
+void loop(){
 }
